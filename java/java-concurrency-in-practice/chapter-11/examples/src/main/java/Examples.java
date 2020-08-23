4a40b6ce9067403907660fbcff3731e912b2be05
Preslav Mihaylov <preslav@uber.com>
2020-08-23 14:09:02 +0300

    add examples for chapter 11 of jcip

---
+++ java/java-concurrency-in-practice/chapter-11/examples/src/main/java/Examples.java
@@ -0,0 +1,64 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.regex.Pattern;
+
+public class Examples {
+    public interface LockScopeExample {
+        void addUserLocation(String user, String loc);
+        boolean userLocationMatches(String user, String regexp);
+    }
+
+    public static class BigLockScopeExample implements LockScopeExample {
+        private final Map<String, String> locations = new HashMap<>();
+
+        public synchronized void addUserLocation(String user, String loc) {
+            locations.put(user, loc);
+        }
+
+        public synchronized boolean userLocationMatches(String user, String regexp) {
+            String loc = locations.get(user);
+            if (loc == null)
+                return false;
+            else
+                return Pattern.matches(regexp, loc);
+        }
+    }
+
+    public static class SmallLockScopeExample implements LockScopeExample {
+        private final Map<String, String> locations = new HashMap<>();
+
+        public synchronized void addUserLocation(String user, String loc) {
+            locations.put(user, loc);
+        }
+
+        public boolean userLocationMatches(String user, String regexp) {
+            String location;
+            synchronized (this) {
+                location = locations.get(user);
+            }
+
+            if (location == null)
+                return false;
+            else
+                return Pattern.matches(regexp, location);
+        }
+    }
+
+    public static class LockStrippingExample implements LockScopeExample {
+        // ConcurrentHashMap implements lock stripping. No need to implement your own :))
+        private final Map<String, String> locations = new ConcurrentHashMap<>();
+
+        public void addUserLocation(String user, String loc) {
+            locations.put(user, loc);
+        }
+
+        public boolean userLocationMatches(String user, String regexp) {
+            String location = locations.get(user);
+            if (location == null)
+                return false;
+            else
+                return Pattern.matches(regexp, location);
+        }
+    }
+}
---
+++ java/java-concurrency-in-practice/chapter-11/examples/src/main/java/Main.java
@@ -0,0 +1,85 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.*;
+
+public class Main extends Thread {
+    private static ThreadLocal<Integer> globalCnt = ThreadLocal.withInitial(() -> 0);
+
+    public static void main(String[] args) throws InterruptedException, BrokenBarrierException {
+        measureLockScopeExample(new Examples.BigLockScopeExample(), 16);
+        measureLockScopeExample(new Examples.SmallLockScopeExample(), 16);
+        measureLockScopeExample(new Examples.LockStrippingExample(), 16);
+    }
+
+    public static void measureLockScopeExample(Examples.LockScopeExample example, int threadsCnt)
+            throws InterruptedException, BrokenBarrierException {
+        ExecutorService exec = Executors.newFixedThreadPool(threadsCnt);
+
+        BlockingQueue<Long> queue = new LinkedBlockingQueue<>();
+        CyclicBarrier barrier = new CyclicBarrier(threadsCnt+1);
+        for (int i = 0; i < threadsCnt; i++) {
+            exec.execute(() -> {
+                List<String> users = new ArrayList<>();
+                List<String> patterns = new ArrayList<>();
+                for (int j = 0; j < 10000; j++) {
+                    users.add(UUID.randomUUID().toString());
+                    example.addUserLocation(users.get(users.size() - 1), UUID.randomUUID().toString());
+                    patterns.add(UUID.randomUUID().toString());
+                }
+
+                try {
+                    barrier.await();
+                } catch (InterruptedException e) {
+                    currentThread().interrupt();
+                } catch (BrokenBarrierException e) {
+                    e.printStackTrace();
+                }
+
+                long start = System.nanoTime();
+                int seed = (int)System.nanoTime();
+                for (int j = 0; j < 50000; j++) {
+                    int rand1 = nextRand(seed) % users.size();
+                    seed = nextRand(seed);
+
+                    int rand2 = nextRand(seed) % users.size();
+                    seed = nextRand(seed);
+
+                    example.userLocationMatches(users.get(rand1), patterns.get(rand2));
+                }
+
+                long total = (System.nanoTime() - start) / 1000000; // milliseconds
+                try {
+                    barrier.await();
+                    queue.put(total);
+                } catch (InterruptedException e) {
+                    currentThread().interrupt();
+                } catch (BrokenBarrierException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+
+        barrier.await();
+        barrier.await();
+
+        exec.shutdown();
+        exec.awaitTermination(5, TimeUnit.SECONDS);
+        int averageTime = 0;
+        for (int i = 0; i < threadsCnt; i++) {
+            averageTime += queue.take();
+        }
+
+        System.out.println(example.getClass().getName() +
+                " - average time per task = " + averageTime/threadsCnt + "ms");
+    }
+
+    private static int nextRand(int y) {
+        y ^= (y << 6);
+        y ^= (y >>> 21);
+        y ^= (y << 7);
+
+        return Math.abs(y);
+    }
+
+}
